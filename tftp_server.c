a7bdd523f2edd58786c0a67d975f2f4259e075a0
Rambod Rahmani <rambodrahmani@autistici.org>
2019-10-21 19:23:00 +0200

    Add TFTP Server and Client source code files.

---
+++ tftp_server.c
@@ -0,0 +1,21 @@
+/**
+ * File: tftp_server.c
+ *       TFTP Server.
+ * 
+ * Author: Rambod Rahmani <rambodrahmani@autistici.org>
+ *         Created on 21/10/2019.
+ */
+
+/**
+ * Entry point.
+ *
+ * @param  argc  command line arguments counter.
+ * @param  argv  command line arguments.
+ *
+ * @return       execution exit code.
+ */
+int main(int argc, char * argv[])
+{
+    return 0;
+}
+
