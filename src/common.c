bf4dab2ceb00265e4b3804bb6b742eb362a80152
Rambod Rahmani <rambodrahmani@autistici.org>
2020-01-02 01:18:15 +0100

    Clean up the source code. Remove redundant comments. Improve indentation.

--- src/common.c
+++ src/common.c
@@ -1,67 +1,66 @@
 /**
  * File: common.c
- *       Common Source File.
+ *	 Common Source File.
  * 
  * Author: Rambod Rahmani <rambodrahmani@autistici.org>
- *         Created on 24/10/2019.
+ *	   Created on 24/10/2019.
  */
 
 #include "../include/common.h"
 
+/**
+ * Info log messages are preceded by the starting character '>' while error log
+ * messages are preceded by the starting character '!>'.
+ */
 void print_log(LogType type, const char *message)
 {
-    // check the given type
-    switch(type)
-    {
-        // info log message
-        case INFO:
-            {
-                fprintf(stdout, "> %s \n", message);
-                break;
-            }
+	switch (type) {
+	case INFO:
+		{
+			fprintf(stdout, "> %s \n", message);
+			break;
+		}
 
-        // error log message
-        case ERROR:
-            {
-                fprintf(stderr, "!> %s \n", message);
-                break;
-            }
-    }
+	case ERROR:
+		{
+			fprintf(stderr, "!> %s \n", message);
+			break;
+		}
+	}
 }
 
+/**
+ * Info log messages are preceded by the starting character '-->' while error
+ * log messages are preceded by the starting character '--!>'.
+ */
 void child_log(LogType type, const char *message)
 {
-    // check the given type
-    switch(type)
-    {
-        // info log message
-        case INFO:
-            {
-                fprintf(stdout, "--> %s \n", message);
-                break;
-            }
+	switch (type) {
+	case INFO:
+		{
+			fprintf(stdout, "--> %s \n", message);
+			break;
+		}
 
-        // error log message
-        case ERROR:
-            {
-                fprintf(stderr, "--!> %s \n", message);
-                break;
-            }
-    }
+	case ERROR:
+		{
+			fprintf(stderr, "--!> %s \n", message);
+			break;
+		}
+	}
 }
 
-void check_errno(int ret, char * info)
+void check_errno(int ret, char *info)
 {
-    // check for errors
-    if (ret <= 0)
-    {
-        // errors occurred, print a warning error message
-        sprintf(log_message, "An unexpected error happened: info = %s; errno = "
-                             "%d", info, errno);
-        print_log(ERROR, log_message);
+	if (ret <= 0)
+	{
+		sprintf(log_message,
+			"An unexpected error happened: info = %s; errno = "
+			"%d", info, errno);
 
-        // quit with errors
-        exit(-1);
-    }
+		print_log(ERROR, log_message);
+
+		exit(-1);
+	}
 }
 
